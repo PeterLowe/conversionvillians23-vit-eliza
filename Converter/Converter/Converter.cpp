58d52585928324281b0525f953139d35ecd1902c
Vit Prechtl <C00298335@setu.ie>
2023-11-17 09:48:38 +0000

    printMenu-VitPrechtl

--- Converter/Converter/Converter.cpp
+++ Converter/Converter/Converter.cpp
@@ -1,8 +1,23 @@
 //Name: Vit Prechtl
 
+#include <iostream>
+
+char printMenu()
+{
+	char choice = ' '; //User input choice
+	std::cout << "Please enter a upper case letter to convert from left to right\And lower to convert from right to left" << std::endl;
+	std::cout << "[A] Degrees <---> Radians [a]" << std::endl;
+	std::cout << "[B] Centimeters <---> Feet & Inches [b]" << std::endl;
+	std::cout << "[C] Litres <---> Gallons [c]" << std::endl;
+
+	std::cout << "[Q] Exit [---] Exit [q]";
+		std::cout << "Enter your choice";
+	std::cin >> choice;
+	return choice;
+}
+
 int main()
 {
-
 
 	return 0;
 }
